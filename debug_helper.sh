7754ebf48862d1b3da215397b05b5e7f547d4602
thankevan <>
2020-09-07 19:14:25 -0400

    Add initial tutorial

---
+++ debug_helper.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# better debugging
+
+export PS4='Script[$0]:Line[$LINENO]:: '
+
+#for more debugging, use this PS4 instead:
+#export PS4='Script $0:Line $LINENO:BASH_LINENO ${BASH_LINENO[@]}:Func ${FUNCNAME[@]}:BASH_SOURCE ${BASH_SOURCE[@]}::'
+
+set -x
+
+echo "PS4 debugging demo script"
+
+#more debugging uncomment this:
+#function inner_func() {
+#  echo "PS4 debugging demo script"
+#}
+#
+#function outer_func() {
+#  inner_func
+#}
+#
+#outer_func
+#
